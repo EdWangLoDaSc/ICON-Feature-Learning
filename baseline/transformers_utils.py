3e8d9ec5eec93a288a8dcd516fbb667de4c5d396
WANG Hanyang <102576088+EdWangLoDaSc@users.noreply.github.com>
2024-11-08 08:56:25 +0800

    Update transformers_utils.py

--- baseline/transformers_utils.py
+++ baseline/transformers_utils.py
@@ -53,3 +53,43 @@ class SelfAttnTransformer(nn.Module):
       x = nn.LayerNorm()(x)
     
     return x
+
+
+#创建数据
+def build_matrices_from_data_shape(data_shape, mode, shot_num_min, return_shape_list = False):
+  '''
+  data_shape is the shape of data, usually obtained by tree.tree_map(lambda x: x.shape, data)
+  '''
+  demo_num = data_shape.demo_cond_k[0]
+  demo_cond_len = data_shape.demo_cond_k[1]
+  demo_qoi_len = data_shape.demo_qoi_k[1]
+  quest_cond_len = data_shape.quest_cond_k[1]
+  quest_qoi_len = data_shape.quest_qoi_k[1]
+
+  cond_bool_list, qoi_kv_bool_list, qoi_k_bool_list = mu.build_bool_sequence(demo_num, mode, shot_num_min)
+  cond_len_list_raw = [demo_cond_len] * demo_num + [quest_cond_len]
+  qoi_kv_len_list_raw = [demo_qoi_len] * demo_num + [quest_qoi_len]
+  qoi_k_len_list_raw = [demo_qoi_len] * demo_num + [quest_qoi_len]
+  cond_len_list = [i * j for i, j in zip(cond_bool_list, cond_len_list_raw)]
+  qoi_kv_len_list = [i * j for i, j in zip(qoi_kv_bool_list, qoi_kv_len_list_raw)]
+  qoi_k_len_list = [i * j for i, j in zip(qoi_k_bool_list, qoi_k_len_list_raw)]
+  
+  basic_mask = mu.build_basic_mask(cond_len_list = cond_len_list, 
+                                qoi_kv_len_list = qoi_kv_len_list, 
+                                qoi_k_len_list = qoi_k_len_list)
+  index_pos = mu.build_index_integer(cond_len_list= cond_len_list,
+                                      qoi_kv_len_list = qoi_kv_len_list,
+                                      qoi_k_len_list = qoi_k_len_list)
+  out_mask = mu.build_out_mask(cond_len_list = cond_len_list,
+                          qoi_kv_len_list= qoi_kv_len_list,
+                          qoi_k_len_list = qoi_k_len_list,
+                          num_range = (shot_num_min, demo_num + 1))
+
+  
+  if return_shape_list:
+    return basic_mask, index_pos, out_mask, cond_len_list, qoi_kv_len_list, qoi_k_len_list
+  else:
+    return basic_mask, index_pos, out_mask
+
+
+
